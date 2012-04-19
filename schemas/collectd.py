9a2171a1d19f150e3f886b7a786353d42ff81fc8
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-04-19 09:52:05 +0000

    Dependency installation.

--- schemas/collectd.py
+++ schemas/collectd.py
@@ -1,6 +1,7 @@
 class Collectd(Schema):
     url = 'http://collectd.org/files/collectd-5.0.2.tar.bz2'
     homepage = 'http://collectd.org/'
+    deps = ['wget', 'bzip2']
 
     def install(self):
         self.call('./configure --prefix={prefix}')
