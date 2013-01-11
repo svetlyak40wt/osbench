1a887e16084fe954b0e88598765ac88c89edfedf
Alexander Artemenko <svetlyak.40wt@gmail.com>
2013-01-11 12:26:56 +0000

    Added schema for 'circus' watchdog.

---
+++ schemas/circus.py
@@ -0,0 +1,14 @@
+class Circus(PythonSchema):
+    url = 'circus==0.6.0'
+    homepage = 'http://circus.readthedocs.org/'
+
+    def install(self):
+        self.makedirs(
+            'etc/init.d',
+            'etc/circus.d',
+            'var/run',
+            'var/log',
+        )
+        self.copy_file('{schema_dir}/configs/etc/init.d/circus', 'etc/init.d/circus', mode=755)
+        self.copy_file('{schema_dir}/configs/etc/circus.conf', 'etc/circus.conf')
+
