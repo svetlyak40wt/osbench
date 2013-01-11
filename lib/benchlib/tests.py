33c74c5a90b7ea4480efcab42a40246d97966f7d
Alexander Artemenko <svetlyak.40wt@gmail.com>
2013-01-11 13:34:30 +0000

    Test in progress

--- lib/benchlib/tests.py
+++ lib/benchlib/tests.py
@@ -16,7 +16,13 @@ def test_install():
 def test_uninstall():
     bench = Bench()
 
+    # there is already python in the bin directory
+    python_filename = os.path.join(bench.osbench_root, 'bin', 'python')
+    # with open(python_filename, 'w') as f:
+    #     f.write('OSBench\'s python')
+
     bench.install('test')
+
     schema = bench.uninstall('test')
 
     linkname = os.path.join(bench.osbench_root, 'bin', 'hello-world.sh')
@@ -24,3 +30,7 @@ def test_uninstall():
 
     assert not os.path.exists(linkname)
     assert not os.path.exists(filename)
+    assert os.path.exists(python_filename)
+
+    with open(python_filename, 'w') as f:
+        assert f.readline() == 'OSBench\'s python'
--- schemas/test.py
+++ schemas/test.py
@@ -5,6 +5,10 @@ class Test(Schema):
     def install(self):
         self.makedirs('bin')
         self.create_file_with_content(
+            'bin/python',
+            'Test project python interpreter'
+        )
+        self.create_file_with_content(
             'bin/hello-world.sh',
             'echo "Hello World!"'
         )
