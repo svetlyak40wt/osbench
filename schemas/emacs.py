f801dbe5f01fde1ee2b36325130a144127cd5c57
Alexander Artemenko <svetlyak.40wt@gmail.com>
2015-11-27 12:20:20 +0300

    Build emacs without X11

--- schemas/emacs.py
+++ schemas/emacs.py
@@ -1,10 +1,10 @@
 class Emacs(Schema):
-    url = 'http://ftp.gnu.org/pub/gnu/emacs/emacs-24.3.tar.gz'
+    url = 'http://ftp.gnu.org/pub/gnu/emacs/emacs-24.5.tar.gz'
     homepage = 'http://www.gnu.org/software/emacs/'
     deps = ['system.libncurses5-dev', 'system.make']
 
     def install(self):
         self.call('./configure --prefix={prefix} '
-                  '--with-xpm=no --with-gif=no --with-tiff=no')
+                  '--with-xpm=no --with-gif=no --with-tiff=no --with-x-toolkit=no')
         self.call('make')
         self.call('make install')
